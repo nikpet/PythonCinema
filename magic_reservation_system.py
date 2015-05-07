e8bfd5af9991d0c049800fe2fbc8df8886e7cfb5
NikolaTsolov <vikingnik@abv.bg>
2015-05-07 20:47:40 +0300

    Adding create_table

---
+++ magic_reservation_system.py
@@ -0,0 +1,20 @@
+import sqlite3
+import sys
+from Database import Database
+
+class CLI:
+    def the_magic(self):
+        while True:
+            reader = input(">")
+
+
+
+
+
+
+
+def main():
+    cli = CLI()
+
+if __name__ == '__main__':
+    main()
