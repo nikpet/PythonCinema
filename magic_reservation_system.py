01047c9a2495328c83624418d6343485ba2fbb22
NikolaTsolov <vikingnik@abv.bg>
2015-05-08 18:28:02 +0300

    Starting work on make_reserv method

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -1,10 +1,26 @@
 from database import Database
+import sys
 
 class CLI:
     def __init__(self):
         self.cinema = Database("cinema.db")
+        self.username = ""
+        self.tickets = 0
+        self.options = {}
+
+    STEPS = [
+        "STEP 1",
+        "STEP 2",
+        "STEP 3",
+        "STEP 3"
+    ]
+
+    def step_one(self):
+        self.username = input("Choose name>")
+        self.tickets = input("Choose number of tickets>")
 
     def show_movies(self):
+        print("Current movies:")
         for movie in self.cinema.show_movies():
             print("[{}] - {} ({})".format(movie[0], movie[1], movie[2]))
 
@@ -18,11 +34,15 @@ class CLI:
                 massage = "[{}] - {} {} ({}) - {} spots available"
                 print(massage.format(proj[0], proj[1], proj[2], proj[3], proj[4]))
 
+    def make_reservation(self):
+        self.step_one()
+        self.show_movies()
+        movie = input("Choose a movie>")
+        self.show_movie_projections(movie)
+
 def main():
     i_o = CLI()
-    i_o.show_movies()
-    i_o.show_movie_projections(1, '2014-04-01')
-    i_o.show_movie_projections(2)
+    i_o.make_reservation()
 
 
 if __name__ == '__main__':
