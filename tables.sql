4b273249a7bb23cb134b171128bf6281b90d58a4
Nikolai Petkov <nikipetkov@abv.bg>
2015-05-08 14:24:21 +0300

    Added " " around dates in sql inserts

--- tables.sql
+++ tables.sql
@@ -32,12 +32,12 @@ INSERT INTO Movies(movie_name, rating) VALUES("The Hunger Games: Catching Fire",
 INSERT INTO Movies(movie_name, rating) VALUES("Wreck-It Ralph", 7.8);
 INSERT INTO Movies(movie_name, rating) VALUES("Her", 8.3);
 
-INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "3D", 2014-04-01, "19:10");
-INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "2D", 2014-04-01, "19:00");
-INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "4DX", 2014-04-02, "21:00");
-INSERT INTO Projections(movie_id, type, date, time) VALUES(3, "2D", 2014-04-05, "20:20");
-INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "3D", 2014-04-02, "22:00");
-INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "2D", 2014-04-02, "19:30");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "3D", "2014-04-01", "19:10");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "2D", "2014-04-01", "19:00");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(1, "4DX", "2014-04-02", "21:00");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(3, "2D", "2014-04-05", "20:20");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "3D", "2014-04-02", "22:00");
+INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "2D", "2014-04-02", "19:30");
 
 INSERT INTO Reservations(username, projection_id, row, col) VALUES("RadoRado", 1, 2, 1);
 INSERT INTO Reservations(username, projection_id, row, col) VALUES("RadoRado", 1, 3, 5);
