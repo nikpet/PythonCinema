473dc3055b84f4cd01d65dc51f4a6715981c5242
Nikolai Petkov <nikipetkov@abv.bg>
2015-05-08 14:02:26 +0300

    Changed plural to sigular form for id keys in database

--- tables.sql
+++ tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE Movies(
 );
 
 CREATE TABLE Projections(
-  projections_id INTEGER PRIMARY KEY,
+  projection_id INTEGER PRIMARY KEY,
   movie_id INTEGER,
   type TEXT,
   date DATE,
@@ -20,12 +20,12 @@ CREATE TABLE Projections(
 );
 
 CREATE TABLE Reservations(
-  reservations_id INTEGER PRIMARY KEY,
+  reservation_id INTEGER PRIMARY KEY,
   username TEXT,
-  projections_id INTEGER,
+  projection_id INTEGER,
   row INTEGER,
   col INTEGER,
-  FOREIGN KEY(projections_id) REFERENCES Projections(projections_id)
+  FOREIGN KEY(projection_id) REFERENCES Projections(projection_id)
 );
 
 INSERT INTO Movies(movie_name, rating) VALUES("The Hunger Games: Catching Fire", 7.9);
@@ -39,10 +39,10 @@ INSERT INTO Projections(movie_id, type, date, time) VALUES(3, "2D", 2014-04-05, 
 INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "3D", 2014-04-02, "22:00");
 INSERT INTO Projections(movie_id, type, date, time) VALUES(2, "2D", 2014-04-02, "19:30");
 
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("RadoRado", 1, 2, 1);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("RadoRado", 1, 3, 5);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("RadoRado", 1, 7, 8);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("Ivo", 3, 1, 1);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("Ivo", 3, 1, 2);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("Mysterious", 5, 2, 3);
-INSERT INTO Reservations(username, projections_id, row, col) VALUES("Mysterious", 5, 2, 4);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("RadoRado", 1, 2, 1);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("RadoRado", 1, 3, 5);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("RadoRado", 1, 7, 8);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("Ivo", 3, 1, 1);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("Ivo", 3, 1, 2);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("Mysterious", 5, 2, 3);
+INSERT INTO Reservations(username, projection_id, row, col) VALUES("Mysterious", 5, 2, 4);
