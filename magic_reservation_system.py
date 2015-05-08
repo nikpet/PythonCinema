790c057c252c17af912eaa1671f9da533828b24d
NikolaTsolov <vikingnik@abv.bg>
2015-05-08 16:26:06 +0300

    Adding movoing_projection method

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -8,9 +8,21 @@ class CLI:
         for movie in self.cinema.show_movies():
             print("[{}] - {} ({})".format(movie[0], movie[1], movie[2]))
 
+    def show_movie_projections(self, movie_id, date=None):
+        if date is not None:
+            for proj in self.cinema.show_movie_projection(movie_id, date):
+                massage = "[{}] - {} ({}) - {} spots available"
+                print(massage.format(proj[0], proj[2], proj[3], proj[4]))
+        else:
+            for proj in self.cinema.show_movie_projection(movie_id):
+                massage = "[{}] - {} {} ({}) - {} spots available"
+                print(massage.format(proj[0], proj[1], proj[2], proj[3], proj[4]))
+
 def main():
     i_o = CLI()
     i_o.show_movies()
+    i_o.show_movie_projections(1, '2014-04-01')
+    i_o.show_movie_projections(2)
 
 
 if __name__ == '__main__':
